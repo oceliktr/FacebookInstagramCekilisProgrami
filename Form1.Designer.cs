594d13405ac184375aa212c351ff69c1d692a0fb
Osman Çelik <osmanceliktr@hotmail.com>
2019-12-06 07:23:42 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtSonuclar = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kopyalaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ndAsil = new System.Windows.Forms.NumericUpDown();
+            this.ndYedek = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFacebook = new System.Windows.Forms.TextBox();
+            this.txtInstagram = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ndAsil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ndYedek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(661, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(463, 548);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(174, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 550);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(376, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Çekilişe Katılan Kişi Sayısı :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 58);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Çekilişi Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtSonuclar
+            // 
+            this.txtSonuclar.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtSonuclar.Location = new System.Drawing.Point(376, 41);
+            this.txtSonuclar.Multiline = true;
+            this.txtSonuclar.Name = "txtSonuclar";
+            this.txtSonuclar.Size = new System.Drawing.Size(269, 552);
+            this.txtSonuclar.TabIndex = 8;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kopyalaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // kopyalaToolStripMenuItem
+            // 
+            this.kopyalaToolStripMenuItem.Name = "kopyalaToolStripMenuItem";
+            this.kopyalaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kopyalaToolStripMenuItem.Text = "Kopyala";
+            this.kopyalaToolStripMenuItem.Click += new System.EventHandler(this.kopyalaToolStripMenuItem_Click);
+            // 
+            // ndAsil
+            // 
+            this.ndAsil.Location = new System.Drawing.Point(10, 12);
+            this.ndAsil.Name = "ndAsil";
+            this.ndAsil.Size = new System.Drawing.Size(51, 20);
+            this.ndAsil.TabIndex = 9;
+            this.ndAsil.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // ndYedek
+            // 
+            this.ndYedek.Location = new System.Drawing.Point(10, 46);
+            this.ndYedek.Name = "ndYedek";
+            this.ndYedek.Size = new System.Drawing.Size(51, 20);
+            this.ndYedek.TabIndex = 10;
+            this.ndYedek.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "asıl";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "yedek";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 58);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Çekilişi Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(658, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "HTML Kodları";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Facebook";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "İnstagram";
+            // 
+            // txtFacebook
+            // 
+            this.txtFacebook.Location = new System.Drawing.Point(9, 149);
+            this.txtFacebook.Name = "txtFacebook";
+            this.txtFacebook.Size = new System.Drawing.Size(158, 20);
+            this.txtFacebook.TabIndex = 17;
+            this.txtFacebook.Text = "profileLink";
+            // 
+            // txtInstagram
+            // 
+            this.txtInstagram.Location = new System.Drawing.Point(9, 188);
+            this.txtInstagram.Name = "txtInstagram";
+            this.txtInstagram.Size = new System.Drawing.Size(158, 20);
+            this.txtInstagram.TabIndex = 18;
+            this.txtInstagram.Text = "FPmhX notranslate  TlrDj";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(7, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 70);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Kullanıcılar için kullanılan class değişkenini yazınız";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 605);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtInstagram);
+            this.Controls.Add(this.txtFacebook);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ndYedek);
+            this.Controls.Add(this.ndAsil);
+            this.Controls.Add(this.txtSonuclar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Erzurum Portalı Çekiliş Programı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ndAsil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ndYedek)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSonuclar;
+        private System.Windows.Forms.NumericUpDown ndAsil;
+        private System.Windows.Forms.NumericUpDown ndYedek;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kopyalaToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFacebook;
+        private System.Windows.Forms.TextBox txtInstagram;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct LinkItem
+        {
+            public string Href;
+            public string Text;
+
+            public override string ToString()
+            {
+                return Href + "\n\t" + Text;
+            }
+        }
+        static class LinkFinder
+        {
+            public static List<LinkItem> Find(string file,string faceboox,string instagram)
+            {
+                List<LinkItem> list = new List<LinkItem>();
+
+                // 1.
+                // Find all matches in file.
+                MatchCollection m1 = Regex.Matches(file, @"(<a.*?>.*?</a>)", RegexOptions.Singleline);
+
+                // 2.
+                // Loop over each match.
+                foreach (Match m in m1)
+                {
+                    string value = m.Groups[1].Value;
+                    //                  instagram     facebook  isim etiketindeki tag bilgisi
+                    if (value.Contains(instagram) || value.Contains(faceboox))  //profileLink
+                    {
+                        LinkItem i = new LinkItem();
+
+                        // 3.
+                        // Get href attribute.
+                        Match m2 = Regex.Match(value, @"href=\""(.*?)\""", RegexOptions.Singleline);
+                        if (m2.Success)
+                        {
+                            i.Href = m2.Groups[1].Value;
+                        }
+
+                        // 4.
+                        // Remove inner tags from text.
+                        string t = Regex.Replace(value, @"\s*<.*?>\s*", "", RegexOptions.Singleline);
+                        i.Text = t;
+
+                        list.Add(i);
+                    }
+                }
+                return list;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string metin = richTextBox1.Text;
+            listBox1.Items.Clear();
+            txtSonuclar.Text = "";
+            // 2.
+            foreach (LinkItem i in LinkFinder.Find(metin,txtFacebook.Text,txtInstagram.Text))
+            {
+                if (i.Text != "Fan Klüb" && i.Text != "Erzurum" && i.Text != "Erzurum'u Keşfe Çıktık!" &&
+                    i.Text != "Osman Çelik" && i.Text != "Said Erzene" && i.Text != "erzurumportali"&&i.Text!="erzurumportakali")
+                {
+                    listBox1.Items.Add(i.Text);
+                }
+            }
+            string[] arr = new string[listBox1.Items.Count];
+            listBox1.Items.CopyTo(arr, 0);
+
+            var arr2 = arr.Distinct();
+
+            listBox1.Items.Clear();
+            foreach (string s in arr2)
+            {
+                listBox1.Items.Add(s); 
+          //      txtSonuclar.Text +=  s + Environment.NewLine;
+            }
+
+            label1.Text = "Çekilişe Katılan Kişi Sayısı :" + listBox1.Items.Count;
+            txtSonuclar.Text += "--------ASILLAR---------" + Environment.NewLine;
+            txtSonuclar.Text += Environment.NewLine;
+
+            for (int j = 1; j <= ndAsil.Value; j++)
+            {
+              Random rnd = new Random();
+                int i = rnd.Next(listBox1.Items.Count);
+                
+                txtSonuclar.Text += j + " - " + listBox1.Items[i] + Environment.NewLine;
+                listBox1.Items.Remove(listBox1.Items[i]);
+            }
+            txtSonuclar.Text += Environment.NewLine;
+            txtSonuclar.Text += "--------YEDEKLER---------" + Environment.NewLine;
+            txtSonuclar.Text += Environment.NewLine;
+
+            for (int y = 1; y <= ndYedek.Value; y++)
+            {
+                Random rnd = new Random();
+                int i = rnd.Next(listBox1.Items.Count);
+
+                txtSonuclar.Text += y + " - " + listBox1.Items[i] + Environment.NewLine;
+                listBox1.Items.Remove(listBox1.Items[i]);
+            }
+            //Clipboard.SetText(txtSonuclar.Text);
+
+        }
+
+        private void kopyalaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSonuclar.Text))
+                Clipboard.SetText(txtSonuclar.Text);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Çekilişe Katılan Kişi Sayısı :" + listBox1.Items.Count;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtSonuclar.Text = "";
+            label1.Text = "Çekilişe Katılan Kişi Sayısı :" + listBox1.Items.Count;
+            txtSonuclar.Text += "--------ASILLAR---------" + Environment.NewLine;
+            txtSonuclar.Text += Environment.NewLine;
+
+                Random rnd = new Random();
+            for (int j = 1; j <= ndAsil.Value; j++)
+            {
+                int i = rnd.Next(0,listBox1.Items.Count);
+          
+                txtSonuclar.Text += j + " - "+ listBox1.Items[i] + Environment.NewLine;
+                listBox1.Items.Remove(listBox1.Items[i]);
+            }
+            txtSonuclar.Text += Environment.NewLine;
+            txtSonuclar.Text += "--------YEDEKLER---------" + Environment.NewLine;
+            txtSonuclar.Text += Environment.NewLine;
+
+            for (int y = 1; y <= ndYedek.Value; y++)
+            {
+                int i = rnd.Next(0,listBox1.Items.Count);
+
+                txtSonuclar.Text += y + " - " + listBox1.Items[i] + Environment.NewLine;
+                listBox1.Items.Remove(listBox1.Items[i]);
+            }
+         //   Clipboard.SetText(txtSonuclar.Text);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
